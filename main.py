58846495756ecf16b6355226c4871b66a4ea6f7b
asubaruwrxsti <60890408+asubaruwrxsti@users.noreply.github.com>
2022-03-15 12:18:38 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-sum = 0
-for i in range(8, -1, -1):
-    sum += 1 / (2 ** i)
-    print("cnt is:", i)
-    print("i is:", 2 ** i)
-    print("div is 1/{:} = {:}".format(2 ** i, 1 / (2 ** i)))
-    print("sum is: {:} + next({:})".format(sum - 1 / (2 ** i), 1 / (2 ** i)))
-    print("\n")
